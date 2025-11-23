37292e8897d466de94184fa0fc50ad8241539ff0
jerffmar <156699949+jerffmar@users.noreply.github.com>
2025-11-23 02:33:52 +0000

    modified:   index.html 	modified:   tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    './portifolio-gabriel/index.html',
+    './portifolio-gabriel/src/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
     extend: {},
   },
